dbdb7d460be2431f62811fc194b632266d8771d1
threeislands <haskell.raspberry@gmail.com>
2022-06-23 07:41:50 +0900

    21.go fix

--- src/21.go
+++ src/21.go
@@ -1,7 +1,5 @@
 package src
 
-import "fmt"
-
 /**
  * Definition for singly-linked list.
  * type ListNode struct {
@@ -14,8 +12,7 @@ func mergeTwoLists(list1 *ListNode, list2 *ListNode) *ListNode {
 	root := ListNode{}
 	current := &root
 	for list1 != nil || list2 != nil {
-		fmt.Print('s')
-		if list2 == nil || list1.Val >= list2.Val {
+		if list2 == nil || (list1 != nil && list1.Val <= list2.Val) {
 			current.Next = &ListNode{Val: list1.Val}
 			current = current.Next
 			list1 = list1.Next
